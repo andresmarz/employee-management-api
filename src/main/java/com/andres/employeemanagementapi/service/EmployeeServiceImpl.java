6dbc8fb652b0be62bcd26d7ab93268499060ccac
andresmarz <andresmarquezzuleta@gmail.com>
2026-05-28 13:19:17 -0400

    Add class EmployeeServiceImpl

---
+++ src/main/java/com/andres/employeemanagementapi/service/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.andres.employeemanagementapi.service;
+
+import com.andres.employeemanagementapi.entity.Employee;
+import com.andres.employeemanagementapi.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}

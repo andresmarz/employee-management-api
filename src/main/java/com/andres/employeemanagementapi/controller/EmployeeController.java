210bcb75bb85005af6e052ff43f3c59cb444023d
andresmarz <andresmarquezzuleta@gmail.com>
2026-05-28 17:16:15 -0400

    Add class EmployeeController

---
+++ src/main/java/com/andres/employeemanagementapi/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.andres.employeemanagementapi.controller;
+
+import com.andres.employeemanagementapi.entity.Employee;
+import com.andres.employeemanagementapi.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+}
